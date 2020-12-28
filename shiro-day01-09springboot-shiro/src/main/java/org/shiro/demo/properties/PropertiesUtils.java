82229d75a2824f014c3e206caec5940efa7b403b
Marsvvv <364848766@qq.com>
2020-12-28 11:20:52 +0800

    finish properitesUtils

--- shiro-day01-09springboot-shiro/src/main/java/org/shiro/demo/properties/PropertiesUtils.java
+++ shiro-day01-09springboot-shiro/src/main/java/org/shiro/demo/properties/PropertiesUtils.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * properties 工具类
@@ -18,7 +16,7 @@ public class PropertiesUtils {
      *
      * @return Properties
      */
-    public static Properties readProperties(String path) {
+    /*public static Properties readProperties(String path) {
         Properties properties = new Properties();
         try (
                 InputStream inputStream = PropertiesUtils.class.getResourceAsStream(path);
@@ -29,19 +27,17 @@ public class PropertiesUtils {
             e.printStackTrace();
         }
         return properties;
-    }
-
-    /**
-     * 将properties配置信息转化为map形式
-     *
-     * @param properties properties
-     * @return Map
-     */
-    public static Map<String, String> properties2Map(Properties properties) {
-        Map<String, String> map = new HashMap<>(properties.entrySet().size());
-        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-            map.put(entry.getKey().toString(), entry.getValue().toString());
+    }*/
+    public static Properties readProperties(String path) {
+        LinkedProperties properties = new LinkedProperties();
+        try (
+                InputStream inputStream = PropertiesUtils.class.getResourceAsStream(path);
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)
+        ) {
+            properties.load(inputStreamReader);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return map;
+        return properties;
     }
 }
